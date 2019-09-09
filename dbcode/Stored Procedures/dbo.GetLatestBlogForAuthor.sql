5f9e735ccb07e484302f3933b23b8317d2a4ea4c
Steve Jones <sjones@sqlservercentral.com>
2019-09-09 14:03:16 +0100

    Commit 1

--- dbcode/Stored Procedures/dbo.GetLatestBlogForAuthor.sql
+++ dbcode/Stored Procedures/dbo.GetLatestBlogForAuthor.sql
@@ -2,12 +2,23 @@ SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
+
 CREATE PROCEDURE [dbo].[GetLatestBlogForAuthor]
-  @authorid INT
-  AS
-  BEGIN
-	SELECT TOP 1 *
-	 FROM dbo.Articles AS a INNER JOIN dbo.Contacts AS c ON c.ContactsID = a.AuthorID
-	 WHERE a.AuthorID = @authorid
-  END
+    @authorid INT,
+    @top TINYINT
+AS
+BEGIN
+    SELECT TOP (@top)
+           a.ArticlesID,
+           a.AuthorID,
+           c.ContactFullName,
+           c.CountryCode,
+           c.Email
+    FROM dbo.Articles       AS a
+    INNER JOIN dbo.Contacts AS c
+        ON c.ContactsID = a.AuthorID
+    WHERE a.AuthorID = @authorid
+    ORDER BY a.PublishDate DESC;
+
+END;
 GO
