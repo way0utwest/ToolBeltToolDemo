d076a34bcceb132c507727d72086d3a6bd71b9e2
Steve Jones <sjones@sqlservercentral.com>
2019-09-09 14:16:56 +0100

    commit 4

--- dbcode/Tables/dbo.Articles.sql
+++ dbcode/Tables/dbo.Articles.sql
@@ -11,7 +11,8 @@ CREATE TABLE [dbo].[Articles]
 [Comments] [int] NOT NULL CONSTRAINT [df_Zero] DEFAULT ((0)),
 [ReadingTimeEstimate] [time] NULL,
 [CreatedDate] [datetime2] (3) NOT NULL,
-[ModifiedBy] [nvarchar] (200) COLLATE SQL_Latin1_General_CP1_CI_AS NULL CONSTRAINT [DF__Articles__Modifi__3F3159AB] DEFAULT (suser_name())
+[ModifiedBy] [nvarchar] (200) COLLATE SQL_Latin1_General_CP1_CI_AS NULL CONSTRAINT [DF__Articles__Modifi__3F3159AB] DEFAULT (suser_name()),
+[Rating] [numeric] (10, 4) NULL
 ) ON [PRIMARY]
 GO
 SET QUOTED_IDENTIFIER ON
@@ -29,4 +30,14 @@ BEGIN
 END
 GO
 ALTER TABLE [dbo].[Articles] ADD CONSTRAINT [PK_Article] PRIMARY KEY CLUSTERED  ([ArticlesID]) ON [PRIMARY]
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Article text for all content', 'SCHEMA', N'dbo', 'TABLE', N'Articles', NULL, NULL
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'FK to Contact.ContactsID', 'SCHEMA', N'dbo', 'TABLE', N'Articles', 'COLUMN', N'AuthorID'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Abstract', 'SCHEMA', N'dbo', 'TABLE', N'Articles', 'COLUMN', N'Description'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'DO NOT REPORT', 'SCHEMA', N'dbo', 'TABLE', N'Articles', 'COLUMN', N'ModifiedDate'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Latest Publish date', 'SCHEMA', N'dbo', 'TABLE', N'Articles', 'COLUMN', N'PublishDate'
 GO
